a31374498a112d632a661d5a05540391f22cab3e
Jeremy-Fowler <jembyfowler@gmail.com>
2026-05-28 13:39:58 -0600

    early stages of calculator

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,4 +1,33 @@
 <script setup lang="ts">
+import { onMounted, ref } from 'vue';
+
+onMounted(() => { addEventListener('keydown', handleKeyDown) })
+
+const keys = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '.', '0', 'Backspace']
+
+const total = ref('')
+
+function updateTotal(key: string) {
+  if (!keys.includes(key)) return
+
+  switch (key) {
+    case '.':
+      if (total.value.includes('.')) return
+      total.value += key
+      break
+    case 'Backspace':
+      if (total.value.length == 0) return
+      total.value = total.value.slice(0, -1)
+      break
+    default:
+      total.value += key
+  }
+}
+
+function handleKeyDown(event: KeyboardEvent) {
+  console.log(event)
+  updateTotal(event.key)
+}
 
 </script>
 
@@ -6,8 +35,15 @@
 <template>
   <div class="container">
     <div class="row">
-      <div class="col-12">
-
+      <div class="col-12 mb-3">
+        <div class="glass-card text-center display-2 py-2">
+          ${{ total }}
+        </div>
+      </div>
+    </div>
+    <div class="row">
+      <div class="col-4 mb-3" v-for="key in keys" :key="'calculator-key-' + key">
+        <button @click="updateTotal(key)" class="btn btn-light w-100" type="button">{{ key }}</button>
       </div>
     </div>
   </div>
